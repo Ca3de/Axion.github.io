4cb552dffd1d852dc143a27197e11a64d045a71d
Ca3de <chengadc@gmail.com>
2024-11-17 03:14:07 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+// Toggle Navigation Menu on Hamburger Click
+const hamburger = document.querySelector('.hamburger');
+const navLinks = document.querySelector('.nav-links');
+
+hamburger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
