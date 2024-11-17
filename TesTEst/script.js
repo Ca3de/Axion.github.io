6bce2c86811606b0123a69efa2aae39c58460bd8
Ca3de <chengadc@gmail.com>
2024-11-17 03:13:53 -0500

    Delete TesTEst directory

--- TesTEst/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle Navigation Menu on Hamburger Click
-const hamburger = document.querySelector('.hamburger');
-const navLinks = document.querySelector('.nav-links');
-
-hamburger.addEventListener('click', () => {
-    navLinks.classList.toggle('active');
-});
